68a7706d88f0bfdc9742ded9cc8c2a45ffa8fe07
1F35C <95659230+1F35C@users.noreply.github.com>
2022-01-24 13:41:37 -0500

    Use React to make navbar is-active

--- src/App.tsx
+++ src/App.tsx
@@ -29,34 +29,41 @@ function NavBar (params: NavBarParams) {
 	let analyticsClass = "navbar-item" + (params.selected === AppTab.Analytics ? " is-active" : "");
 	let dataClass = "navbar-item" + (params.selected === AppTab.Data ? " is-active" : "");
 	let aboutClass = "navbar-item" + (params.selected === AppTab.About ? " is-active" : "");
+  let [burger, setBurger] = useState<boolean>(false);
 
 	useEffect(() => {
-		document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-			// Get all "navbar-burger" elements
-			const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
 
-			// Check if there are any navbar burgers
-			if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach( burger => {
+        burger.addEventListener('click', () => {
 
-				// Add a click event on each of them
-				$navbarBurgers.forEach( burger => {
-					burger.addEventListener('click', () => {
+          // Get the target from the "data-target" attribute
+          const target = burger.dataset.target;
+          const $target = document.getElementById(target);
 
-						// Get the target from the "data-target" attribute
-						const target = burger.dataset.target;
-						const $target = document.getElementById(target);
-
-						// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-						burger.classList.toggle('is-active');
-						if ($target) {
-							$target.classList.toggle('is-active');
-						}
-					});
-				});
-			}
-		});
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          setBurger(!burger);
+          burger.classList.toggle('is-active');
+          if ($target) {
+            $target.classList.toggle('is-active');
+          }
+        });
+			});
+		}
 	}, []);
+
+  let navbarButtonClass = "navbar-burger";
+  let navbarMenuClass = "navbar-menu";
+
+  if (burger) {
+    navbarButtonClass += " is-active";
+    navbarMenuClass += " is-active";
+  }
 
   /* eslint-disable jsx-a11y/anchor-is-valid */
   return (
@@ -66,14 +73,14 @@ function NavBar (params: NavBarParams) {
 					<img src="ak-factions/logo_rhine.png" alt="" /><span className="strong">Rhine Lab</span>&nbsp;Data Archive
 				</span>
 
-				<a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+				<a role="button" className={ navbarButtonClass } aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 				</a>
 			</div>
 
-			<div id="navbarBasicExample" className="navbar-menu">
+			<div id="navbarBasicExample" className={ navbarMenuClass }>
 				<div className="navbar-start">
 					<a className={ homeClass } onClick={ params.goToHome }>
 					  Home
